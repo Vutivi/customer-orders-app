4e7d60f9fbd4210c2b771daddaa9755fa7a406cd
Vutivi <201141790@student.uj.ac.za>
2018-06-08 12:20:32 +0200

    test heroku bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   resources :suppliers
   resources :employees
   resources :customers
- root 'welcome#index' # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index' # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
